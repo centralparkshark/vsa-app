a0ef02861565b9de37520854a5a7b1a1588ba283
centralparkshark <mikecupcake13@gmail.com>
2024-02-29 15:08:32 -0500

    Added Firebase deploy set up

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -37,20 +37,20 @@ const AuthScreen = () => {
   return (
     <div className="bento justify-center items-center h-screen">
         <div className="box shadow-md w-80">
-        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <h2 className="text-2xl font-semibold text-right w-full">Login</h2>
         <form>
             <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email</label>
+                <label htmlFor="email" className="block text-gray-400 font-medium mb-1">Email</label>
                 <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-400" placeholder="Enter your email" defaultValue={initForm.email} onChange={handleChange}></input>
             </div>
             <div className="mb-4">
-                <label htmlFor="password" className="block text-gray-700 font-medium mb-1">Password</label>
-                <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-400" placeholder="Enter your password" defaultValue={initForm.password} onChange={handleChange}></input>
+                <label htmlFor="password" className="block text-gray-400 font-medium mb-1">Password</label>
+                <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-400" placeholder="********" defaultValue={initForm.password} onChange={handleChange}></input>
             </div>
             <button 
                 disabled={loading || !initForm.email.trim() || !initForm.password.trim()} 
                 onClick={handleAuth} 
-                className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 transition duration-200 disabled:bg-gray-400"
+                className="w-1/2 bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 transition duration-200 disabled:bg-gray-400"
                 >Login</button>
         </form> 
     </div>
